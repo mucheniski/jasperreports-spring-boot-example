83d093f6acda5c2e8b0bc248527fd08bc9cb13a5
mucheniski <mucheniski@gmail.com>
2021-03-31 14:58:18 -0300

    First Commit

---
+++ src/test/java/com/example/jasperreportsspringbootexample/JasperreportsSpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jasperreportsspringbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperreportsSpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
